23fb77bec2ec322764e2bc2495cc7035b5927a6c
Lorenzo Rondan <dp1septemberlrd@gmail.com>
2020-04-15 14:23:23 +0200

    Cambios backend S3

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    apiKey: "AIzaSyACwgpXMFnOinp2VPGAR7z-9YLb4NlMDXs",
-    authDomain: "ispp-99815.firebaseapp.com",
-    databaseURL: "https://ispp-99815.firebaseio.com",
-    projectId: "ispp-99815",
-    storageBucket: "ispp-99815.appspot.com",
-    messagingSenderId: "191301845231",
-    appId: "1:191301845231:web:5fbd26378419e96e8edb1a",
-    measurementId: "G-HK9V8XJM42",
+    apiKey: "AIzaSyC9ycD9EfrxCYfKThb-v-QZoMk-CKt6Eus",
+    authDomain: "biblioshare-s3.firebaseapp.com",
+    databaseURL: "https://biblioshare-s3.firebaseio.com",
+    projectId: "biblioshare-s3",
+    storageBucket: "biblioshare-s3.appspot.com",
+    messagingSenderId: "19810316786",
+    appId: "1:19810316786:web:00b48b6d03f1d3cbaf3b6b",
+    measurementId: "G-GLYJGB03DG",
     user: "biblioshareClient@gmail.com", 
     pass: "biblio123!"
 };
