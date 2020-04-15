935f9fff685e0f88373058eac520163e1d9fd6cf
Lorenzo Rondan <dp1septemberlrd@gmail.com>
2020-04-15 22:36:21 +0200

    Arreglo CORS

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,12 @@ var cors = require('cors');
 var app = express();
 app.use(cors({ origin: true}));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const { db } = require('./util/admin');
 const config = require('./util/config');
 const firebase = require('firebase');
